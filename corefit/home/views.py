4ef1f3666a20ea21da865837363abe6ef515ba97
Igor <igor-peres@hotmail.com>
2018-06-13 07:01:53 -0300

    1.01

--- corefit/home/views.py
+++ corefit/home/views.py
@@ -3,8 +3,7 @@ from .models import teste
 
 # Create your views here.
 def home(request):
-    if request.method == 'POST':
-        name = request.POST.get(name)
-        img = request.POST.get(img)
-
-    return render(request, 'home/home.html', {'title':'Home'})
+    if request.user.is_authenticated():
+        return render(request, 'home/home.html', {'title':'Home'})
+    else:
+        return render(request, 'home/erro.html', {'title':'Erro'})
