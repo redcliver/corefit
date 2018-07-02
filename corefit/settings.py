ee94d2d1f509ea6171b6385eae6b697bca226f0c
Igor <igor-peres@hotmail.com>
2018-07-02 18:49:25 -0400

    1.01

--- corefit/settings.py
+++ corefit/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f483ec50-3070-41cb-bb68-447bda887083'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'corefit.herokuapp.com',
