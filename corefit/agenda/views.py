018861dbb180da54dc3259697e8079b7313480b5
Igor <igor-peres@hotmail.com>
2018-07-02 09:37:55 -0400

    1.01

--- corefit/agenda/views.py
+++ corefit/agenda/views.py
@@ -3,8 +3,19 @@ from django.utils.timezone import datetime
 # Create your views here.
 def agenda1(request):
     if request.user.is_authenticated():
-        hoje = datetime.today().strftime('%A, %d de %B de %Y')
+        hoje = datetime.today().strftime('%d de %B de %Y')
         dia_sem = datetime.today().strftime('%A, %d de %B de %Y')
-        return render(request, 'agenda.html', {'title':'Agenda', 'hoje':hoje})
+        sem = datetime.today().strftime('%a')
+        if sem == 'Mon':
+            dia = 1
+        elif sem == 'Tue':
+            dia = 2
+        elif sem == 'Wed':
+            dia = 3
+        elif sem == 'Thu':
+            dia = 4
+        elif sem == 'Fri':
+            dia = 5
+        return render(request, 'agenda.html', {'title':'Agenda', 'hoje':hoje, 'dia':dia})
     else:
         return render(request, 'home/erro.html', {'title':'Erro'})
